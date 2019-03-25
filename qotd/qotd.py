7b1a96e73d67f00e57ad2a1521fe0303d2e6f349
John Gaines #2633 <47873376+JohnGaines@users.noreply.github.com>
2019-03-25 13:26:02 -0700

    finally fixed indentation for this

--- qotd/qotd.py
+++ qotd/qotd.py
@@ -1,18 +1,12 @@
 from redbot.core import commands
-<<<<<<< HEAD:qotd/qotd.py
 
-class Mycog:
-=======
-        
-class qotd:
->>>>>>> 4d1f631a9ccacf6ba5d87bb3f49c38e0292fffee:qotd/qotd.py
+class qotd(commands.Cog):
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
 
-     @commands.command()
-  async def mycom(self, ctx):
-      """This does stuff!"""
-      # Your code will go here
-      await ctx.send("I can do stuff!")
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        await ctx.send("I can do stuff!")
